577522e697a09040e95e667a381c1ff84c0071f4
Maxi <herreramaxi@gmail.com>
2021-09-12 14:58:20 +0100

    creating api/temperature endpoint for post requests

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const path = require('path');
 const db = require('./database/models/index');
 const TemperatureSample = db.TemperatureSample;
 
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded());
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
 if (process.env.NODE_ENV !== 'production') {
     app.use(cors());
 }
@@ -19,7 +25,7 @@ app.get('/', function (req, res) {
 
 app.get('/hello', (req, res) => {
     res.send("hello world!")
-})
+});
 
 app.get('/getTemperatureSamples', (req, res) => {
 
@@ -30,7 +36,22 @@ app.get('/getTemperatureSamples', (req, res) => {
             res.send(e);
         });
 
-})
+});
+
+app.post("/api/temperature", (req, res) => {
+    if (!req.body.temperature)
+        return res.status(400).send({ message: "Mandatory fields not provided: [temperature]" });
+
+    try {
+
+        TemperatureSample.create({ value: req.body.temperature, sampleDate: new Date() })
+            .then(r => {
+                res.send(r);
+            });
+    } catch (error) {
+        res.status(500).send({ message: "Error when trying to save a temperature sample. " + error })
+    }
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 3001);
